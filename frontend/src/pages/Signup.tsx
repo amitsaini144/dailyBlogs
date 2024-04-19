2a04b08974ce5e82b71978944c8829d45c9c4285
Amit Saini <1amit24saini5@gmail.com>
2024-04-20 03:21:20 +0530

    Update the name

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -54,7 +54,7 @@ export default function Signup() {
             
                 <div className="max-w-md w-full mx-auto rounded-xl md:rounded-2xl p-4 md:p-8 pb-1 md:pb-1 shadow-input bg-white dark:bg-black">
                     <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                        Welcome to Medium
+                        Welcome to Daily Blogs
                     </h2>
                     <p className="text-neutral-600 text-sm max-w-sm mt-2 mr-10 dark:text-neutral-300">
                         Enter your information to create an account
